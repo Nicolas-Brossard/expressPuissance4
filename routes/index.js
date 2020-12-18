fb6015160dbfcb2a604e334a68ec647e2d3de88e
Nathan PIGNON <nathan.pignon79@gmail.com>
2020-12-18 12:10:15 +0100

    Tableau des scores

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const Score = [{'player':'Premier', 'score':3},{'player':'Deuxième', 'score':5},{'player':'Troisième', 'score':9}]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { Score: Score });
 });
 
 module.exports = router;
